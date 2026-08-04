5475b1f8dae401a375ac3b9101a00fe4a8e2c301
Filza-150 <filzanaeem987@gmail.com>
2026-08-04 14:14:23 +0500

    btn theme change

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 function circle() {
     let circle1 = document.getElementById("toggleCircle");
-    let container1 = document.getElementById("container")
+    let container1 = document.getElementById("container");
+    let themeToggle = document.getElementById("theme-toggle");
     circle1.classList.toggle("circle-right");
     // circle1.style.left = "50px"
-    container1.classList.toggle("dark")
+    container1.classList.toggle("dark");
+    themeToggle.classList.toggle("btn-dark")
+
 }
