05dd5e35420ab4c1827f348e4605470785dbadcc
Filza-150 <filzanaeem987@gmail.com>
2026-08-04 21:48:24 +0500

    make increasing and decreasing btn

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 function circle() {
-    let circle1 = document.getElementById("toggleCircle");
-    let container1 = document.getElementById("container");
-    let themeToggle = document.getElementById("theme-toggle");
-    circle1.classList.toggle("circle-right");
-    // circle1.style.left = "50px"
-    container1.classList.toggle("dark");
-    themeToggle.classList.toggle("btn-dark")
+  let circle1 = document.getElementById("toggleCircle");
+  let container1 = document.getElementById("container");
+  let themeToggle = document.getElementById("theme-toggle");
+  let increaseNumbers = document.getElementById("change-numbers");
+  circle1.classList.toggle("circle-right");
+  // circle1.style.left = "50px"
+  container1.classList.toggle("dark");
+  themeToggle.classList.toggle("btn-dark");
+  increaseNumbers.classList.toggle("number-mode")
 
+
+}
+
+let count = 0
+let increaseNumbers = document.getElementById("change-numbers");
+
+function plusBtn() {
+    count++;
+  increaseNumbers.innerHTML = count;
+  if (count === 101) {
+    count = 100
+    increaseNumbers.innerHTML = count;
+  } 
+}
+
+function minusBtn() {
+    count--;
+    increaseNumbers.innerHTML = count;
 }
