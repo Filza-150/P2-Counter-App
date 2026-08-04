c611e63e1266908ceea6a08b0207de588aef6ff7
Filza-150 <filzanaeem987@gmail.com>
2026-08-04 14:05:42 +0500

    theme-toggle complete

---
+++ script.js
@@ -0,0 +1,7 @@
+function circle() {
+    let circle1 = document.getElementById("toggleCircle");
+    let container1 = document.getElementById("container")
+    circle1.classList.toggle("circle-right");
+    // circle1.style.left = "50px"
+    container1.classList.toggle("dark")
+}
